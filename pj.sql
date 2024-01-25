55e21f47c0ffeca3bdfd7c643bfa32bc149be1c3
naufalkhairyy <naufal22si@mahasiswa.pcr.ac.id>
2024-01-25 11:13:46 +0700

    Database

--- pj.sql
+++ pj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2024 at 08:29 PM
+-- Generation Time: Jan 25, 2024 at 05:12 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,10 +41,9 @@ CREATE TABLE `absensi` (
 INSERT INTO `absensi` (`absensi_kelas`, `absensi_tanggal`, `absensi_keterangan`, `absensi_id`) VALUES
 ('2 SI A', '2024-01-08', 'MTK', 99),
 ('2 SI A', '2024-01-18', 'TES', 100),
-('2 SI A', '2024-01-23', 'asdf', 101),
-('2 SI A', '2024-01-19', 'ASasf', 102),
-('2 SI B', '2024-01-25', 'ASFASF', 103),
-('2 SI B', '2024-01-20', 'ASD', 104);
+('2 SI A', '2024-01-02', 'MTK', 105),
+('2 SI C', '2024-01-17', 'PBO', 107),
+('2 SI A', '2024-01-19', 'MTk', 108);
 
 -- --------------------------------------------------------
 
@@ -64,37 +63,47 @@ CREATE TABLE `absensi_siswa` (
 --
 
 INSERT INTO `absensi_siswa` (`absensisiswa_id`, `id_siswa`, `absensi_id`, `keterangan`) VALUES
-('1cd97a58-ad92-11ee-a6c5-e4a8dfe2ac58', '1634d9d1-2ac7-4923-840f-2dba9b80af92', '103', 'Hadir'),
-('1cd9a999-ad92-11ee-a6c5-e4a8dfe2ac58', '216e552c-4dee-43d8-bc2d-93a21155346c', '103', 'Hadir'),
-('1cd9dc9a-ad92-11ee-a6c5-e4a8dfe2ac58', '51297137-51a6-44f2-8970-6b8a860afdc8', '103', 'Hadir'),
-('1cda0ce0-ad92-11ee-a6c5-e4a8dfe2ac58', '84eb1073-3da2-4f50-a32d-4c8b068b5b36', '103', 'Hadir'),
-('1cda387d-ad92-11ee-a6c5-e4a8dfe2ac58', 'a655bc5b-2ef4-4a7b-884b-7665b764444e', '103', 'Hadir'),
-('1cdaabe1-ad92-11ee-a6c5-e4a8dfe2ac58', 'd0eb28ab-52f0-4ef4-ac2d-76418637c93f', '103', 'Hadir'),
-('51e47cd0-ad91-11ee-a6c5-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '100', 'Hadir'),
-('51e4b27f-ad91-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '100', 'Hadir'),
-('51e4ec84-ad91-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '100', 'Hadir'),
-('51e54ab9-ad91-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '100', 'Hadir'),
-('51e57550-ad91-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '100', 'Hadir'),
-('a0aafb38-ad92-11ee-a6c5-e4a8dfe2ac58', '1634d9d1-2ac7-4923-840f-2dba9b80af92', '104', 'Hadir'),
-('a0ab31d5-ad92-11ee-a6c5-e4a8dfe2ac58', '216e552c-4dee-43d8-bc2d-93a21155346c', '104', 'Hadir'),
-('a0ab6a3a-ad92-11ee-a6c5-e4a8dfe2ac58', '51297137-51a6-44f2-8970-6b8a860afdc8', '104', 'Alpha'),
-('a0ab9f9a-ad92-11ee-a6c5-e4a8dfe2ac58', '84eb1073-3da2-4f50-a32d-4c8b068b5b36', '104', 'Sakit'),
-('a0abcd83-ad92-11ee-a6c5-e4a8dfe2ac58', 'a655bc5b-2ef4-4a7b-884b-7665b764444e', '104', 'Alpha'),
-('a0ac695f-ad92-11ee-a6c5-e4a8dfe2ac58', 'd0eb28ab-52f0-4ef4-ac2d-76418637c93f', '104', 'Hadir'),
-('d786fcbd-ad91-11ee-a6c5-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '101', 'Hadir'),
-('d78736b8-ad91-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '101', 'Hadir'),
-('d7877659-ad91-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '101', 'Hadir'),
-('d787a961-ad91-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '101', 'Hadir'),
-('d7881ccd-ad91-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '101', 'Hadir'),
-('dd0949b0-ad91-11ee-a6c5-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '102', 'Hadir'),
-('dd0985a6-ad91-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '102', 'Alpha'),
-('dd09b5ae-ad91-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '102', 'Hadir'),
-('dd09e20c-ad91-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '102', 'Alpha'),
-('dd0a0e25-ad91-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '102', 'Hadir'),
-('f43c8a2c-ad8e-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '99', 'Hadir'),
-('f43cb57b-ad8e-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '99', 'Hadir'),
-('f43d0d26-ad8e-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '99', 'Hadir'),
-('f43d3ea9-ad8e-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '99', 'Hadir');
+('1cd97a58-ad92-11ee-a6c5-e4a8dfe2ac58', '1634d9d1-2ac7-4923-840f-2dba9b80af92', '103', 'ASFASF'),
+('1cd9a999-ad92-11ee-a6c5-e4a8dfe2ac58', '216e552c-4dee-43d8-bc2d-93a21155346c', '103', 'ASFASF'),
+('1cd9dc9a-ad92-11ee-a6c5-e4a8dfe2ac58', '51297137-51a6-44f2-8970-6b8a860afdc8', '103', 'ASFASF'),
+('1cda0ce0-ad92-11ee-a6c5-e4a8dfe2ac58', '84eb1073-3da2-4f50-a32d-4c8b068b5b36', '103', 'ASFASF'),
+('1cda387d-ad92-11ee-a6c5-e4a8dfe2ac58', 'a655bc5b-2ef4-4a7b-884b-7665b764444e', '103', 'ASFASF'),
+('1cdaabe1-ad92-11ee-a6c5-e4a8dfe2ac58', 'd0eb28ab-52f0-4ef4-ac2d-76418637c93f', '103', 'ASFASF'),
+('289adfe7-ae20-11ee-a029-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '106', 'Alpha'),
+('289b177d-ae20-11ee-a029-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '106', 'Hadir'),
+('289b41ad-ae20-11ee-a029-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '106', 'Alpha'),
+('289ba2b3-ae20-11ee-a029-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '106', 'Sakit'),
+('289bcc1c-ae20-11ee-a029-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '106', 'Hadir'),
+('4ec57bef-ae20-11ee-a029-e4a8dfe2ac58', '34201a36-4da0-4164-a663-536c61ee3a85', '107', 'Sakit'),
+('4ec5bc7b-ae20-11ee-a029-e4a8dfe2ac58', '4ad21690-89ab-4683-91b6-7ee1f44c124c', '107', 'Hadir'),
+('4ec5e6e9-ae20-11ee-a029-e4a8dfe2ac58', 'c34e3744-4840-486f-b6e0-afa472749c85', '107', 'Hadir'),
+('4ec60ef3-ae20-11ee-a029-e4a8dfe2ac58', 'cdd5a802-5ed2-4500-be33-68818953cb59', '107', 'Alpha'),
+('4ec66f8f-ae20-11ee-a029-e4a8dfe2ac58', 'd6efdbe4-c08f-4498-93af-a90a38d01c36', '107', 'Hadir'),
+('51e47cd0-ad91-11ee-a6c5-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '100', 'TES'),
+('51e4b27f-ad91-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '100', 'TES'),
+('51e4ec84-ad91-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '100', 'TES'),
+('51e54ab9-ad91-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '100', 'TES'),
+('51e57550-ad91-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '100', 'TES'),
+('61355213-ae20-11ee-a029-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '108', 'Alpha'),
+('61358708-ae20-11ee-a029-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '108', 'Hadir'),
+('6135dde7-ae20-11ee-a029-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '108', 'Hadir'),
+('61360389-ae20-11ee-a029-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '108', 'Hadir'),
+('61362a9e-ae20-11ee-a029-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '108', 'Hadir'),
+('7ba38d98-adcd-11ee-a2f5-e4a8dfe2ac58', '0b640513-e107-49a1-8d0f-45e64266c73e', '105', 'Hadir'),
+('7ba3c73c-adcd-11ee-a2f5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '105', 'Hadir'),
+('7ba42059-adcd-11ee-a2f5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '105', 'Hadir'),
+('7ba447f8-adcd-11ee-a2f5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '105', 'Hadir'),
+('7ba4686f-adcd-11ee-a2f5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '105', 'Hadir'),
+('a0aafb38-ad92-11ee-a6c5-e4a8dfe2ac58', '1634d9d1-2ac7-4923-840f-2dba9b80af92', '104', 'ASD'),
+('a0ab31d5-ad92-11ee-a6c5-e4a8dfe2ac58', '216e552c-4dee-43d8-bc2d-93a21155346c', '104', 'ASD'),
+('a0ab6a3a-ad92-11ee-a6c5-e4a8dfe2ac58', '51297137-51a6-44f2-8970-6b8a860afdc8', '104', 'ASD'),
+('a0ab9f9a-ad92-11ee-a6c5-e4a8dfe2ac58', '84eb1073-3da2-4f50-a32d-4c8b068b5b36', '104', 'ASD'),
+('a0abcd83-ad92-11ee-a6c5-e4a8dfe2ac58', 'a655bc5b-2ef4-4a7b-884b-7665b764444e', '104', 'ASD'),
+('a0ac695f-ad92-11ee-a6c5-e4a8dfe2ac58', 'd0eb28ab-52f0-4ef4-ac2d-76418637c93f', '104', 'ASD'),
+('f43c8a2c-ad8e-11ee-a6c5-e4a8dfe2ac58', '0dd6fcae-3de3-4e0d-9b77-c1fb454264a4', '99', 'MTK'),
+('f43cb57b-ad8e-11ee-a6c5-e4a8dfe2ac58', '6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', '99', 'MTK'),
+('f43d0d26-ad8e-11ee-a6c5-e4a8dfe2ac58', 'd4746845-80e5-41c9-a96e-3b5300289aac', '99', 'MTK'),
+('f43d3ea9-ad8e-11ee-a6c5-e4a8dfe2ac58', 'ec4f1801-3c1b-4b11-8689-76dabae91dde', '99', 'MTK');
 
 -- --------------------------------------------------------
 
@@ -246,6 +255,7 @@ INSERT INTO `siswa1` (`id`, `nama_siswa`, `kelas`, `jenis_kelamin`, `alamat`) VA
 ('4a4ff7cc-3c8e-4981-89ba-085c41a3d5b6', 'SI D', '4', 'Laki-laki', 'OPal'),
 ('4ad21690-89ab-4683-91b6-7ee1f44c124c', 'SI C', '3', 'Laki-laki', 'Tes'),
 ('51297137-51a6-44f2-8970-6b8a860afdc8', 'SI B', '2', 'Laki-laki', 'B'),
+('60213eaf-28bd-4c44-8b19-cafae9f7bc08', 'JEREMY', '4', 'Laki-laki', 'PADANG'),
 ('6522c0f3-4efd-4d5b-b79c-6d5ce0778fb9', 'SI A', '1', 'Laki-laki', 'SI A'),
 ('84eb1073-3da2-4f50-a32d-4c8b068b5b36', 'SI B 4', '2', 'Laki-laki', 'B'),
 ('97d9232e-be0d-4b90-863a-b680dc53f98d', 'SI D 5', '4', 'Laki-laki', 'D'),
@@ -401,7 +411,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `absensi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+  MODIFY `absensi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
